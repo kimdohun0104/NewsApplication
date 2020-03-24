9029c802c06d73b70f115c9c3e3fbdc1e92a642b
kimdohun0104 <dikolight203@gmail.com>
2020-03-24 18:52:04 +0900

    [UPDATE] Splash finish after 1300 milliseconds Use kotlin coroutine for delay 1300 milliseconds

--- app/src/main/java/com/dohun/news/SplashActivity.kt
+++ app/src/main/java/com/dohun/news/SplashActivity.kt
@@ -1,9 +1,24 @@
 package com.dohun.news
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
 
 class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        private const val SPLASH_WAIT_MILLI = 1300L
+    }
+
+    init {
+        lifecycleScope.launchWhenCreated {
+            delay(SPLASH_WAIT_MILLI)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- buildSrc/src/main/java/dependencies/Deps.kt
+++ buildSrc/src/main/java/dependencies/Deps.kt
@@ -9,11 +9,18 @@ object Deps {
     object Kotlin {
         const val version = "1.3.61"
         const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$version"
+        const val coroutineCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5"
     }
 
     object AndroidX {
         const val appCompat = "androidx.appcompat:appcompat:1.1.0"
         const val coreKtx = "androidx.core:core-ktx:1.2.0"
         const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+
+        object LifeCycle {
+            private const val version = "2.2.0"
+            const val lifecycleKtx =  "androidx.lifecycle:lifecycle-runtime-ktx:$version"
+            const val compiler = "androidx.lifecycle:lifecycle-compiler:$version"
+        }
     }
 }
