1130cde8eb4ff981c9161ce42cf83f70f716fe65
kimdohun0104 <dikolight203@gmail.com>
2020-03-25 19:00:17 +0900

    [UPDATE] Local news data source

--- app/src/main/java/com/dohun/news/NewsApplication.kt
+++ app/src/main/java/com/dohun/news/NewsApplication.kt
@@ -1,6 +1,7 @@
 package com.dohun.news
 
 import android.app.Application
+import com.dohun.local.di.localModule
 import com.dohun.remote.di.remoteModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -14,7 +15,8 @@ class NewsApplication : Application() {
             androidContext(this@NewsApplication)
             modules(
                 listOf(
-                    remoteModule
+                    remoteModule,
+                    localModule
                 )
             )
         }
