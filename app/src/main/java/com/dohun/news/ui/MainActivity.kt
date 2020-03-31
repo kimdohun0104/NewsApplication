60f51181a6d4be7447121ccc502bb83f488366a2
kimdohun0104 <dikolight203@gmail.com>
2020-03-31 17:27:24 +0900

    [UPDATE] Back press twice to finish app

--- app/src/main/java/com/dohun/news/ui/MainActivity.kt
+++ app/src/main/java/com/dohun/news/ui/MainActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.dohun.news.R
 import com.dohun.news.databinding.ActivityMainBinding
 import com.dohun.news.ui.adapter.NewsListAdapter
 import com.dohun.news.viewModel.MainViewModel
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +21,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val viewModel: MainViewModel by viewModel()
+
+    private var isBackPressed = false
+
+    override fun onBackPressed() {
+        if (isBackPressed) {
+            super.onBackPressed()
+            return
+        }
+        isBackPressed = true
+        Snackbar.make(binding.clRoot, R.string.message_back_press_to_finish, Snackbar.LENGTH_SHORT).show()
+        lifecycleScope.launch {
+            delay(2000)
+            isBackPressed = false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
